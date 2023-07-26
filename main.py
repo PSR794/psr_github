6725b4ed72ec59911a1b28a1081672a7024eefc9
Prathamesh Ringe <64797216+PSR794@users.noreply.github.com>
2023-07-26 12:54:06 +0530

    mqtt in main.py

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import json
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import esp
+import esp32  
+import json# import all needed libraries
+esp.osdebug(None)     # debug set to none
+import gc             # garbage collector
+gc.collect()
+
+mqtt_server = b"b-4a6fc5ce-85ec-440f-8582-11553261333b-1.mq.ap-south-1.amazonaws.com"
+#mqtt_server = "broker.emqx.io"     # works best for me
+#mqtt_server = "broker.hivemq.com"
+#mqtt_server = '3.65.154.195:1883'  #broker.hivemq.com
+#mqtt_server = '5.196.95.208'       #test.mosquitto.org
+client_id = ubinascii.hexlify(machine.unique_id()) # gets ID
+topic_pub = b'factri/predev/trident/budhni/sheeting/csp/eton1/rpi1/rfid' # toppic name
+
+last_message = 0 
+message_interval = 5
+counter = 0
+
+
 def restart_and_reconnect():
   print('Failed to connect to MQTT broker. Reconnecting...')
   time.sleep(5)
